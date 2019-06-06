89522e8b529bdd2a2c76831419b2a09043b7d7f2
dienkim <trietnk01@gmail.com>
2019-06-06 12:13:39 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/assets/demo-js/owl-carousel-pro.js
+++ wp-content/themes/vidoco-theme/assets/demo-js/owl-carousel-pro.js
@@ -16,4 +16,44 @@ jQuery(document).ready(function(){
             }
         }
     });
+    jQuery(".owl-carousel-brand").owlCarousel({
+        autoplay:false,
+        loop:true,
+        margin:100,
+        nav:true,
+        navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+        dots:false,
+        mouseDrag: true,
+        touchDrag: true,
+        lazyLoad: true,
+        responsiveClass:true,
+        responsive:{
+            240:{
+                items:1,
+                autoplay:true,
+                nav:false,
+            },
+            320:{
+                items:2,
+                margin:100,
+                autoplay:true,
+                nav:false,
+            },
+            740:{
+                margin:50,
+            },
+            768:{
+                items:5,
+                margin:50,
+            },
+            1024:{
+                items:5,
+                margin:80,
+            },
+            1140:{
+                items:5,
+                margin:100,
+            },
+        }
+    });
 });
--- wp-content/themes/vidoco-theme/index.php
+++ wp-content/themes/vidoco-theme/index.php
@@ -27,5 +27,45 @@
         ?>
     </div>
     <?php
+    $args = array(
+        'post_type' => 'zapartner',
+        'orderby' => 'id',
+        'order'   => 'DESC',
+        'posts_per_page' => -1,
+    );
+    $the_query_doi_tac=new WP_Query($args);
+    if($the_query_doi_tac->have_posts()){
+        ?>
+        <div class="box-doi-tac">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="owl-carousel-brand owl-carousel owl-theme owl-loaded">
+                            <?php
+                            while($the_query_doi_tac->have_posts()) {
+                                $the_query_doi_tac->the_post();
+                                $post_id=$the_query_doi_tac->post->ID;
+                                $featured_img=get_the_post_thumbnail_url(@$post_id, 'full');
+                                ?>
+                                <div class="item">
+                                    <div class="brand-item">
+                                        <a href="javascript:void(0);">
+                                            <figure>
+                                                <div style="background-image: url('<?php echo $featured_img; ?>');background-repeat: no-repeat;background-size: cover;padding-top: calc(100% / (200/130));"></div>
+                                            </figure>
+                                        </a>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                            wp_reset_postdata();
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
     get_footer();
     ?>
